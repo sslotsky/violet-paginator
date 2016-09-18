6dc8fbbb71e1634a0bc44a01dfc6aca5d3db6f72
Sam Slotsky <saxosamo@gmail.com>
2016-09-17 23:11:01 -0500

    remove redundancy in actions

--- src/actions.js
+++ src/actions.js
@@ -68,110 +68,78 @@ export default function register(config) {
       return dispatch(execute(action))
     },
     reload: fetch,
-    next: () => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.NEXT_PAGE,
-          id
-        })
-      ),
-    prev: () => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.PREVIOUS_PAGE,
-          id
-        })
-      ),
-    goTo: (page) => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.GO_TO_PAGE,
-          id,
-          page
-        })
-      ),
-    setPageSize: (size) => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.SET_PAGE_SIZE,
-          id,
-          size
-        })
-      ),
-    toggleFilterItem: (field, value) => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.TOGGLE_FILTER_ITEM,
-          id,
-          field,
-          value
-        })
-      ),
-    setFilter: (field, value) => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.SET_FILTER,
-          id,
-          field,
-          value
-        })
-      ),
-    setFilters: (filters) => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.SET_FILTERS,
-          id,
-          filters
-        })
-      ),
-    resetFilters: (filters) => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.RESET_FILTERS,
-          id,
-          filters
-        })
-      ),
-    sort: (field, reverse) => dispatch =>
-      dispatch(
-        execute({
-          type: actionTypes.SORT_CHANGED,
-          id,
-          field,
-          reverse
-        })
-      ),
-    updatingItem: (itemId) => dispatch =>
-      dispatch({
-        type: actionTypes.UPDATING_ITEM,
-        id,
-        itemId
-      }),
-    updateItem: (itemId, data) => dispatch =>
-      dispatch({
-        type: actionTypes.UPDATE_ITEM,
-        id,
-        itemId,
-        data
-      }),
-    removingItem: (itemId) => dispatch =>
-      dispatch({
-        type: actionTypes.REMOVING_ITEM,
-        id,
-        itemId
-      }),
-    removeItem: (itemId) => dispatch =>
-      dispatch({
-        type: actionTypes.REMOVE_ITEM,
-        id,
-        itemId
-      }),
-    itemError: (itemId, error) => dispatch =>
-      dispatch({
-        type: actionTypes.ITEM_ERROR,
-        id,
-        itemId,
-        error
-      })
+    next: () => execute({
+      type: actionTypes.NEXT_PAGE,
+      id
+    }),
+    prev: () => execute({
+      type: actionTypes.PREVIOUS_PAGE,
+      id
+    }),
+    goTo: (page) => execute({
+      type: actionTypes.GO_TO_PAGE,
+      id,
+      page
+    }),
+    setPageSize: (size) => execute({
+      type: actionTypes.SET_PAGE_SIZE,
+      id,
+      size
+    }),
+    toggleFilterItem: (field, value) => execute({
+      type: actionTypes.TOGGLE_FILTER_ITEM,
+      id,
+      field,
+      value
+    }),
+    setFilter: (field, value) => execute({
+      type: actionTypes.SET_FILTER,
+      id,
+      field,
+      value
+    }),
+    setFilters: (filters) => execute({
+      type: actionTypes.SET_FILTERS,
+      id,
+      filters
+    }),
+    resetFilters: (filters) => execute({
+      type: actionTypes.RESET_FILTERS,
+      id,
+      filters
+    }),
+    sort: (field, reverse) => execute({
+      type: actionTypes.SORT_CHANGED,
+      id,
+      field,
+      reverse
+    }),
+    updatingItem: (itemId) => ({
+      type: actionTypes.UPDATING_ITEM,
+      id,
+      itemId
+    }),
+    updateItem: (itemId, data) => ({
+      type: actionTypes.UPDATE_ITEM,
+      id,
+      itemId,
+      data
+    }),
+    removingItem: (itemId) => ({
+      type: actionTypes.REMOVING_ITEM,
+      id,
+      itemId
+    }),
+    removeItem: (itemId) => ({
+      type: actionTypes.REMOVE_ITEM,
+      id,
+      itemId
+    }),
+    itemError: (itemId, error) => ({
+      type: actionTypes.ITEM_ERROR,
+      id,
+      itemId,
+      error
+    })
   }
 }
