ef40fccc11a2310258f2fa296171d73cbc94659d
Sam Slotsky <saxosamo@gmail.com>
2016-12-10 23:45:03 -0600

    Merge preloaded props with defaultPaginator on first load

--- src/lib/stateManagement.js
+++ src/lib/stateManagement.js
@@ -1,7 +1,15 @@
+import Immutable from 'immutable'
 import { defaultPaginator } from '../reducer'
 
 export default function getPaginator(state, listId) {
   return state.pagination.find(p => p.get('id') === listId, undefined, defaultPaginator)
+}
+
+const preload = { results: [] }
+
+export function preloadedPaginator(state, listId, preloaded = preload) {
+  const paginator = getPaginator(state, listId)
+  return paginator.equals(defaultPaginator) ? paginator.merge(preloaded) : paginator
 }
 
 export function isUpdating(state, listId, itemId) {
